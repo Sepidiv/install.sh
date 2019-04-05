40f1b13b866bb82a1d1156abcdc63b9f90f6fe31
Moha <mmjafari@openmailbox.org>
2019-04-05 13:57:23 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ mkdir Sepidiv
 cd Sepidiv
 git clone http://github.com/sepidiv/Argon
 git clone http://github.com/sepidiv/Iodin
-git clone http://github.com/sepidiv/Sodium
+#git clone http://github.com/sepidiv/Sodium
 pwd > ~/.SepiDiv
